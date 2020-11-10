bf280e78fdb9f2c07161288d161e76c1a1af4965
MBYTE2000 <astriyonok@mail.ru>
2020-11-10 21:16:31 +0300

    1
    111

---
+++ Hahaton/Assets/light.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class light : MonoBehaviour
+{
+    public float factor;
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+
+        if (hit.collider != null && hit.collider.tag == "Mirror")
+        {
+            Vector2 dir = Vector2.Reflect(transform.up, hit.normal);
+
+            var mirror = hit.collider.GetComponent<mirror>();
+            mirror.DrawRay(hit.point, dir, factor);
+        }
+        else
+        {
+            mirror.delRays();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(transform.position, transform.up*factor);
+    }
+}
---
+++ Hahaton/Assets/mirror.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mirror : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject luch;
+
+    public List<GameObject> lights = new List<GameObject>();
+    public void DrawRay(Vector3 pos, Vector3 dir, float dist)
+    {
+        Debug.DrawRay(pos, dir * dist, Color.red);
+        var g = Instantiate(luch, pos, Quaternion.identity);
+        g.transform.rotation = Quaternion.Euler(dir);
+        lights.Add(g);
+    }
+
+    public void delRays()
+    {
+        if (lights == null) return;
+        foreach (var i in lights)
+        {
+            Destroy(i);
+        }
+    }
+}
